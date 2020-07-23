378b5097d1539f8c7845aa8bb029bcb2c3a0a43f
Krisztián Tresó <chreex@qgears.com>
2020-07-23 09:58:48 +0200

    updating to APIs bundled with Eclipse 2020-03

--- hu.qgears.repocache/src/hu/qgears/repocache/RepoCache.java
+++ hu.qgears.repocache/src/hu/qgears/repocache/RepoCache.java
@@ -16,6 +16,7 @@ import org.apache.log4j.PatternLayout;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.session.DefaultSessionIdManager;
 import org.eclipse.jetty.server.session.HashSessionIdManager;
 import org.eclipse.jetty.server.session.HashSessionManager;
 import org.eclipse.jetty.server.session.SessionHandler;
@@ -126,16 +127,16 @@ public class RepoCache {
 		server = new Server();
 		
         // Specify the Session ID Manager
-        HashSessionIdManager idmanager = new HashSessionIdManager();
-        server.setSessionIdManager(idmanager);
+        DefaultSessionIdManager sessionIdManager = new DefaultSessionIdManager(server);
+		server.setSessionIdManager(sessionIdManager);
 
         // Sessions are bound to a context.
         ContextHandler context = new ContextHandler("/");
         server.setHandler(context);
 
         // Create the SessionHandler (wrapper) to handle the sessions
-        HashSessionManager manager = new HashSessionManager();
-        SessionHandler sessions = new SessionHandler(manager);
+        SessionHandler sessions = new SessionHandler();
+        sessions.setSessionIdManager(sessionIdManager);
         context.setHandler(sessions);
 
 		ServerConnector sc = new ServerConnector(server);
